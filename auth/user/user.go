63541d46d22ff43da1a762135dfcf79dae6d36ae
Isara Kerdpra <isara.fff@gmail.com>
2024-05-28 01:17:51 +0700

    add user list and func get user

--- auth/user/user.go
+++ auth/user/user.go
@@ -1,6 +1,9 @@
 package auth
 
-import "time"
+import (
+	"errors"
+	"time"
+)
 
 type User struct {
 	Id        int
@@ -10,4 +13,44 @@ type User struct {
 	CreatedAt *time.Time
 	UpdatedAt *time.Time
 	DeletedAt *time.Time
+}
+
+var timeUTC = time.UTC
+var id1Birthday = time.Date(1999, 3, 12, 0, 0, 0, 0, timeUTC)
+var id2Birthday = time.Date(1970, 11, 10, 0, 0, 0, 0, timeUTC)
+var id3Birthday = time.Date(1984, 7, 30, 0, 0, 0, 0, timeUTC)
+
+var userLits = []User{
+	{
+		Id:       1,
+		Username: "bangkoker",
+		Email:    "bangkoker@gmail.com",
+		Birthday: &id1Birthday,
+	},
+	{
+		Id:       2,
+		Username: "chiangmaiker",
+		Email:    "chiangmaiker@gmail.com",
+		Birthday: &id2Birthday,
+	},
+	{
+		Id:       3,
+		Username: "phuketker",
+		Email:    "phuketker@gmail.com",
+		Birthday: &id3Birthday,
+	},
+}
+
+func (u User) Get(id int) (User, error) {
+	var foundUser = User{}
+	for _, user := range userLits {
+		if user.Id == id {
+			foundUser = user
+		}
+	}
+	if foundUser.Id == 0 {
+		return foundUser, errors.New("not found user")
+	}
+
+	return foundUser, nil
 }
